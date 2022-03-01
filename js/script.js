a37aa27a5d2ef6eea6686d81e630ede231c740bc
valentinaabakumova <abakumova.va13@gmail.com>
2022-03-01 12:54:42 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("hi");
+alert("hello");
+
+// const isComing = confirm("Please confirm hotel reservation");
+// console.log(isComing);
+
+// const value = prompt("Please enter a number!");
+// console.log(typeof value); // "string"
+// console.log(value); // "5"
+
+const message = "JavaScript is awesome!";
+console.log(message); // JavaScript is awesome!
