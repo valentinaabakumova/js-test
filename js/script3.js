7d6084ab5a644480cdb0f58781accfc702ad41db
valentinaabakumova <abakumova.va13@gmail.com>
2022-04-04 10:04:24 +0200

    first task

---
+++ js/script3.js
@@ -0,0 +1,29 @@
+console.log("hello, V");
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country ="Jamaica";
+// apartment.location.city = "Kingston";
+
+// console.log(apartment.area);
+// console.log(apartment.location.city);
+// console.log(apartment.location.country);
+
+
+
